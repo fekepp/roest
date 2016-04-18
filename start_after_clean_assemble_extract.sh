b8ea20544560af7f9816197e34908fdaaaaa68a1
Felix Leif Keppmann <felix.leif@keppmann.de>
2016-04-19 01:28:17 +0200

    Use installDist instead of assemble

--- start_after_clean_assemble_extract.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-./gradlew clean assemble && unzip -d roest-server/build/distributions/ roest-server/build/distributions/roest-server-*.zip && ./roest-server/build/distributions/roest-server-*/bin/roest-server
